764796f3e7f1dd33a87487e1a89c228b814b2b7f
sonny <gyeomsony@gmail.com>
2025-01-20 11:07:12 +0900

    🎨[Chore] 프로젝트 생성

---
+++ Sodam/Sodam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sodam
+//
+//  Created by 손겸 on 1/20/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+
+
+}
+

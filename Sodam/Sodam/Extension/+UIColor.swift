c486094820f7140d81bd93af279148050e713cb9
emilyj4482 <emilyj4482@gmail.com>
2025-01-20 13:55:31 +0900

    🎨 [Chore] Color set 코드 추가

---
+++ Sodam/Sodam/Extension/+UIColor.swift
@@ -0,0 +1,18 @@
+//
+//  +UIColor.swift
+//  Sodam
+//
+//  Created by EMILY on 20/01/2025.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static let backgroundColor = UIColor(red: 0.97, green: 0.93, blue: 0.89, alpha: 1.00)
+    
+    static let secondaryColor1 = UIColor(red: 0.87, green: 0.83, blue: 0.76, alpha: 1.00)
+    static let secondaryColor2 = UIColor(red: 0.82, green: 0.72, blue: 0.66, alpha: 1.00)
+    
+    static let primaryColor = UIColor(red: 0.55, green: 0.29, blue: 0.23, alpha: 1.00)
+}

3d32ea25cad10038579b3b67d2ea166c929686a4
Emily <emilyj4482@gmail.com>
2025-01-21 22:18:52 +0900

    ✨ [Feat] 행담이 상태 재사용뷰 생성

---
+++ Sodam/Sodam/HangdamStatusView.swift
@@ -0,0 +1,39 @@
+//
+//  HangdamStatusView.swift
+//  Sodam
+//
+//  Created by EMILY on 21/01/2025.
+//
+
+import SwiftUI
+
+struct HangdamStatusView: View {
+    
+    let mockHangdam = MockHangdam.mockHangdam
+    
+    var body: some View {
+        HStack(alignment: .center, spacing: 16) {
+            Image(.babyHangdam)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .background(Color.imageBackground)
+                .clipShape(.circle)
+            
+            VStack(alignment: .leading, spacing: 10) {
+                Text(mockHangdam.name)
+                    .font(.mapoGoldenPier(24))
+                Text("Lv.\(mockHangdam.level) 애기 행담이")
+                    .font(.mapoGoldenPier(18))
+                Text("\(mockHangdam.startDate?.toString ?? "") ~")
+                    .font(.mapoGoldenPier(17))
+            }
+            .foregroundStyle(Color(uiColor: .white))
+        }
+        .padding(16)
+        .background(Color.tabBackground)
+    }
+}
+
+#Preview {
+    HangdamStatusView()
+}

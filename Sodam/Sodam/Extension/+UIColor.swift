ef3a20dd21dc2f93ccac99c823b3d8baec65edd1
emilyj4482 <emilyj4482@gmail.com>
2025-01-21 16:49:37 +0900

    ✏️ [Modify] color set 수정

--- Sodam/Sodam/Extension/+UIColor.swift
+++ Sodam/Sodam/Extension/+UIColor.swift
@@ -9,10 +9,13 @@ import UIKit
 
 extension UIColor {
     
-    static let backgroundColor = UIColor(red: 0.97, green: 0.93, blue: 0.89, alpha: 1.00)
+    static let backgroundColor = UIColor(red: 0.97, green: 0.93, blue: 0.89, alpha: 1.00)   // view 배경 색상
     
-    static let secondaryColor1 = UIColor(red: 0.87, green: 0.83, blue: 0.76, alpha: 1.00)
-    static let secondaryColor2 = UIColor(red: 0.82, green: 0.72, blue: 0.66, alpha: 1.00)
+    static let quaternaryColor = UIColor(red: 1.00, green: 0.98, blue: 0.98, alpha: 1.00)   // 행담이 image view 배경
+    static let tertiaryColor = UIColor(red: 0.92, green: 0.82, blue: 0.76, alpha: 1.00)     // 행담이 컬렉션 cell 배경
     
-    static let primaryColor = UIColor(red: 0.55, green: 0.29, blue: 0.23, alpha: 1.00)
+    static let secondaryColor = UIColor(red: 0.82, green: 0.72, blue: 0.66, alpha: 1.00)    // 탭바 배경, 행담이 상태 블록 배경
+    
+    static let primaryColor = UIColor(red: 0.76, green: 0.57, blue: 0.45, alpha: 1.00)      // 행복 작성하기 button 배경
+    static let accentColor = UIColor(red: 0.55, green: 0.29, blue: 0.23, alpha: 1.00)       // 제일 진한 갈색 (탭 하이라이트, 타이틀 글자색)
 }

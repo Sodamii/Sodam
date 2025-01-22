d343ff4ab406edab9697f213ca467d4c95bd4cf4
sy0201 <noey0201@gmail.com>
2025-01-22 15:51:11 +0900

    ✨ [Feat] UserDefaultsManager 추가

---
+++ Sodam/Sodam/UserDefaultsManager.swift
@@ -0,0 +1,44 @@
+//
+//  UserDefaultsManager.swift
+//  Sodam
+//
+//  Created by 박시연 on 1/22/25.
+//
+
+import Foundation
+
+final class UserDefaultsManager {
+    static let shared = UserDefaultsManager()
+    private let userDefaults = UserDefaults.standard
+    
+    private init() {}
+    
+    // 이름 충돌 방지 및 재사용성 증가
+    private enum Keys {
+        static let id = "id"
+        static let notificationTime = "Date"
+        static let isToggleNotification = "isToggleNotification"
+        static let content = "content"
+        static let imagePath = "imagePath"
+    }
+    
+    // MARK: - UserDefaults에 저장
+    
+    func saveNotificationTime(_ date: Date) {
+        userDefaults.set(date, forKey: Keys.notificationTime)
+    }
+    
+    func saveIsToggleNotification(_ isOn: Bool) {
+        userDefaults.set(isOn, forKey: Keys.isToggleNotification)
+    }
+    
+    // MARK: - UserDefaults에 저장된 값 얻어오기
+    
+    func getNotificationTime() -> Date? {
+        userDefaults.object(forKey: Keys.notificationTime) as? Date
+    }
+    
+    func getIsToggleNotification() -> Bool {
+        userDefaults.bool(forKey: Keys.isToggleNotification)
+    }
+}

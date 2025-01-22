9c813bcc120f9eb76bdc46f95a2b32eaa4f6a741
sonny <gyeomsony@gmail.com>
2025-01-22 09:36:51 +0900

    ✨ [Feat] 랜덤 문구 정의 및 반환 코드

--- Sodam/Sodam/Main/View/MainMessages.swift
+++ Sodam/Sodam/Main/View/MainMessages.swift
@@ -7,6 +7,32 @@
 
 import Foundation
 
-enum MainMessages: CaseIterable {
-    // 랜덤 문구 정의할 예정
+enum MainMessages: String, CaseIterable {
+    case message1 = "행복은 먼 곳에 있지 않아요. 지금 이 순간에 왔을지도요."
+    case message2 = "작은 것에 감사할 줄 알 때, 큰 행복이 찾아온답니다."
+    case message3 = "당신은 이미 충분히 빛나는 전구 같은 사람!"
+    case message4 = "웃음은 마음의 태양이죠. 오늘도 환하게 웃어보기!"
+    case message5 = "당신이 라면만 먹어도 행복하다면 그게 행복이에요."
+    case message6 = "좋은 생각이 좋은 하루를 만들어준답니다."
+    case message7 = "오늘 하루, 고생 많았어요. 화이팅"
+    case message8 = "작은 친절 하나가 소소한 행복을 만듭니다."
+    case message9 = "행복은 길 위에 놓인 작은 꽃과 같습니다. 걸음을 멈추고 둘러보아요."
+    case message10 = "당신이 사랑받고 있다는 사실을 잊지 말기."
+    case message11 = "소소한 행복은 목적지가 아니라 여정입니다."
+    case message12 = "어제보다 나은 오늘, 그것이 행복이죠. 별거 있나요?"
+    case message13 = "소소한 행복은 언제나 당신 곁에 있어요."
+    case message14 = "작은 성공을 축하하는 건 큰 행복의 시작이랍니다."
+    case message15 = "행복은 다른 사람의 미소를 보는 데서 시작될 수 있답니다."
+    case message16 = "당신의 마음속엔 이미 행복의 씨앗이 있네요."
+    case message17 = "힘들 때일수록 스스로를 응원해주세요. 행복은 당신을 기다립니다."
+    case message18 = "행복은 완벽함이 아닌, 불완전함을 사랑하는 마음에서 오는 것."
+    case message19 = "자신을 소중히 여길 때, 행복은 더 자주 찾아온답니다."
+    case message20 = "지금 느끼는 감사가 내일의 행복을 만들어줄거에요"
+    
+    static let firstMessage = "소소하고 확실한 행복으로 행담이를 깨워볼까요?"
+    
+    // 랜덤으로 메세지 반환하기 (firstMessage는 제외하기)
+    static func getRandomMessage() -> String {
+        return Self.allCases.filter { $0.rawValue != firstMessage }.randomElement()?.rawValue ?? "오늘의 소확행 기록하기."
+    }
 }

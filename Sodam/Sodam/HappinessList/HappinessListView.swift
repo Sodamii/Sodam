9f9623f4a789a4061ba5d1ebf2f04e1505e5296f
jbnong07 <jbnong07@naver.com>
2025-01-22 14:59:50 +0900

    ✨ [Feat] 리스트 뷰 생성

--- Sodam/Sodam/HappinessList/HappinessListView.swift
+++ Sodam/Sodam/HappinessList/HappinessListView.swift
@@ -5,3 +5,36 @@
 //  Created by 박진홍 on 1/21/25.
 //
 
+import SwiftUI
+
+struct HappinessListView: View {
+    
+    let array: [Int] = [12,123,1123]
+    
+    var body: some View {
+        Text("행담이가 먹고 자란 기억들")
+            .font(.mapoGoldenPier(20))
+        List {
+            ForEach(array, id: \.self) { value in
+                HStack {
+                    RoundedRectangle(cornerRadius: 15)
+                        .frame(width: 90, height: 120)
+                    VStack {
+                        Text("Horror House")
+                            .multilineTextAlignment(.leading)
+                        Text("Merong merong")
+                        Text("1102.1230.123")
+                    }
+                    Spacer(minLength: 80)
+                }
+            }
+        }
+        
+    }
+}
+
+#Preview {
+    HappinessListView()
+}
+
+

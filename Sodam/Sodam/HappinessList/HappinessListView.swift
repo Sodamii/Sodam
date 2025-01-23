fcb37286380ca2a71dd7a0d1f670c2902387c87a
jbnong07 <jbnong07@naver.com>
2025-01-23 13:05:10 +0900

    ✨ [Feat] 리스트뷰 세부 조정

--- Sodam/Sodam/HappinessList/HappinessListView.swift
+++ Sodam/Sodam/HappinessList/HappinessListView.swift
@@ -8,33 +8,68 @@
 import SwiftUI
 
 struct HappinessListView: View {
+    @State var mockHangdam: MockHangdam = .mockHangdam
     
-    let array: [Int] = [12,123,1123]
+    private let cornerRadius: CGFloat = 15
+    private var array: [Int] = [1,2,3,4,5] // mock happiness가 hasable하지 않아 list 테스트용 배열 생성함.
     
     var body: some View {
-        Text("행담이가 먹고 자란 기억들")
-            .font(.mapoGoldenPier(20))
-        List {
-            ForEach(array, id: \.self) { value in
-                HStack {
-                    RoundedRectangle(cornerRadius: 15)
-                        .frame(width: 90, height: 120)
-                    VStack {
-                        Text("Horror House")
-                            .multilineTextAlignment(.leading)
-                        Text("Merong merong")
-                        Text("1102.1230.123")
+        GeometryReader { geometry in
+            VStack(alignment: .center) {
+                HangdamStatusView(size: geometry.size, mockHangdam: $mockHangdam)
+                    .clipShape(.rect(cornerRadius: cornerRadius))
+
+                Text("엄마는 아무것도 모른담이가 먹고 자란 기억들")
+                    .frame(maxWidth: .infinity,maxHeight: 35, alignment: .leading)
+                    .font(.mapoGoldenPier(FontSize.title))
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.1)
+                    .foregroundStyle(Color.textAccent)
+                    .padding(.top)
+
+                List {
+                    ForEach(array, id: \.self) { value in
+                        HStack {
+                            Rectangle() // 사진 대체용
+                                .frame(width: 90, height: 120)
+                                .clipShape(.rect(cornerRadius: cornerRadius))
+                            
+                            VStack(alignment: .leading) {
+                                Text("나는 어제 엽떡을 먹어서 행복했다. 근데 오늘 엽떡이 너무 매워서 완전 피똥쌌다. 힘들다. 다시느 ㄴ엽떡을 안 머거야지")
+                                    .font(.mapoGoldenPier(FontSize.body))
+                                    .lineLimit(2)
+                                    .frame(height: 50, alignment: .topLeading)
+                                Text("\(mockHangdam.startDate?.toString ?? "")")
+                                    .font(.mapoGoldenPier(FontSize.timeStamp))
+                                    .foregroundStyle(.gray)
+                            }
+                            .padding(.leading)
+                        }
                     }
-                    Spacer(minLength: 80)
+                    .listRowSeparator(.hidden)
+                    .listRowBackground(
+                        RoundedRectangle(cornerRadius: cornerRadius)
+                            .foregroundStyle(Color.cellBackground)
+                    )
                 }
+                .listRowSpacing(20)
+                .listStyle(.plain)
             }
         }
-        
+        .padding([.top, .horizontal])
+        .background(Color.viewBackground)
+    }
+}
+
+extension HappinessListView {
+    enum FontSize{
+        static let title: CGFloat = 27
+        static let body: CGFloat = 16
+        static let timeStamp: CGFloat = 14
     }
 }
 
 #Preview {
     HappinessListView()
 }
-
 

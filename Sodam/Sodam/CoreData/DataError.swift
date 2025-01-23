c6c9354316ef3c342960483e6e04127854fb37da
Emily <emilyj4482@gmail.com>
2025-01-24 02:46:06 +0900

    ✏️ [Modify] DataError에 imagePaths 컨버팅 실패 case 추가

--- Sodam/Sodam/CoreData/DataError.swift
+++ Sodam/Sodam/CoreData/DataError.swift
@@ -13,6 +13,7 @@ enum DataError: Error {
     case fetchRequestFailed
     case searchEntityFailed
     case convertIDFailed
+    case convertImagePathsFailed
     
     // 디버깅용 print 구문
     var localizedDescription: String {
@@ -22,6 +23,7 @@ enum DataError: Error {
         case .fetchRequestFailed: "[CoreData Error] entity fetch 실패"
         case .searchEntityFailed: "[CoreData Error] entity search 실패"
         case .convertIDFailed: "[CoreData Error] DTO id >>> NSManagedObjectID 변환 실패"
+        case .convertImagePathsFailed: "[CoreData Error] DTO [String] >>> Data 변환 실패"
         }
     }
     
@@ -31,7 +33,7 @@ enum DataError: Error {
         case .containerLoadFailed: "오류로 인해 행담이를 불러오지 못했어요🥲\n앱 종료 후 재실행하거나, 개발자에게 문의해주세요."
         case .contextSaveFailed: "변경 사항 저장에 실패했어요🥲\n 다시 시도하거나, 개발자에게 문의해주세요."
         case .fetchRequestFailed: "데이터를 불러오는 데 실패했어요🥲\n앱 종료 후 재실행하거나, 개발자에게 문의해주세요."
-        case .searchEntityFailed, .convertIDFailed: ""
+        case .searchEntityFailed, .convertIDFailed, .convertImagePathsFailed: "작업에 실패했어요🥲"
         }
     }
 }

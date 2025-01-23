861e9fea2ff7901086819fe8b501e4bd1f08e91b
jbnong07 <jbnong07@naver.com>
2025-01-24 08:44:18 +0900

    ✨ [Feat] Set navigation link to detail view

---
+++ Sodam/Sodam/HappinessDetail/HappinessDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  HappinessDetailView.swift
+//  Sodam
+//
+//  Created by 박진홍 on 1/24/25.
+//
+
+import SwiftUI
+
+struct HappinessDetailView: View {
+    
+    var body: some View {
+        Text("Detail View")
+    }
+}
+
+#Preview {
+    HappinessDetailView()
+}
+

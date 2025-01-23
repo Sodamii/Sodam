92eea5cb7cdc892cc1a76b7ae6d81434dc7e24ad
Emily <emilyj4482@gmail.com>
2025-01-23 13:40:35 +0900

    ✨ [Feat] DataError 추가

---
+++ Sodam/Sodam/CoreData/DataError.swift
@@ -0,0 +1,35 @@
+//
+//  DataError.swift
+//  Sodam
+//
+//  Created by EMILY on 22/01/2025.
+//
+
+import Foundation
+
+enum DataError: Error {
+    case containerLoadFailed
+    case contextSaveFailed
+    case fetchRequestFailed
+    case searchEntityFailed
+    
+    // 디버깅용 print 구문
+    var localizedDescription: String {
+        switch self {
+        case .containerLoadFailed: "[CoreData Error] container 로드 실패"
+        case .contextSaveFailed: "[CoreData Error] context 저장 실패"
+        case .fetchRequestFailed: "[CoreData Error] entity fetch 실패"
+        case .searchEntityFailed: "[CoreData Error] entity search 실패"
+        }
+    }
+    
+    // 사용자에게 띄울 alert에 넣을 구문
+    var alertDescription: String {
+        switch self {
+        case .containerLoadFailed: "오류로 인해 행담이를 불러오지 못했어요🥲\n앱 종료 후 재실행하거나, 개발자에게 문의해주세요."
+        case .contextSaveFailed: "변경 사항 저장에 실패했어요🥲\n 다시 시도하거나, 개발자에게 문의해주세요."
+        case .fetchRequestFailed: "데이터를 불러오는 데 실패했어요🥲\n앱 종료 후 재실행하거나, 개발자에게 문의해주세요."
+        case .searchEntityFailed: ""
+        }
+    }
+}

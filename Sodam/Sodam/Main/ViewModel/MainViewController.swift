f51a5f990876ae23794f63ec9e1907b0826637e3
sonny <gyeomsony@gmail.com>
2025-01-23 10:14:14 +0900

    💄 [Style] 중복코드 정리 및 메서드 이름 수정

--- Sodam/Sodam/Main/ViewModel/MainViewController.swift
+++ Sodam/Sodam/Main/ViewModel/MainViewController.swift
@@ -12,23 +12,19 @@ final class MainViewController: UIViewController {
     // 메인 뷰 인스턴스 생성
     private let mainView = MainView()
     
-    // 작성 버튼을 처음 눌렀는지 판단 (UserDefaults에 저장된 이름이 없으면 true)
-    private var isCreateButtonFirstTap: Bool {
-        return UserDefaults.standard.string(forKey: "HangdamName") == nil
-    }
-    
     override func loadView() {
         self.view = mainView
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureFirstMessage()    // 초기 메세지 설정
-        addGesture()               // 이미지 탭 제스처
+        setupMessage()    // 초기 메세지 설정
         configureButtonAction()    // 작성 버튼 액션 설정
+        addGesture()               // 이미지 탭 제스처
         updateNameLabelIfNeeded()  // 앱 로드 시에 이름 라벨 업데이트
     }
     
+    // 이름 라벨 업데이트
     private func updateNameLabelIfNeeded() {
         if let savedName = UserDefaults.standard.string(forKey: "HangdamName") {
             mainView.updateNameLabel(savedName) // 저장된 이름으로 라벨 업데이트
@@ -38,8 +34,13 @@ final class MainViewController: UIViewController {
         }
     }
     
-    private func configureFirstMessage() {
-        if UserDefaults.standard.string(forKey: "HangdamName") != nil {
+    // 저장된 이름 여부 확인
+    private func isNameSet() -> Bool {
+        return UserDefaults.standard.string(forKey: "HangdamName") != nil
+    }
+    
+    private func setupMessage() {
+        if isNameSet() {
             mainView.updateMessage(MainMessages.getRandomMessage())
             print("이름이 저장된 후 앱 로드되어 랜덤 메세지를 표시")
         } else {
@@ -69,6 +70,42 @@ final class MainViewController: UIViewController {
         present(writeViewController, animated: true)                            // 모달 표시
     }
     
+    // 작성 버튼 클릭 시 호출
+    @objc private func createButtonTapped() {
+        if isNameSet() {
+            // 이미 저장된 이름이 있는 경우에 바로 작성화면으로 이동
+            if let savedName = UserDefaults.standard.string(forKey: "HangdamName") {
+                print("저장된 이름으로 작성화면 이동함: \(savedName)")
+                mainView.updateNameLabel(savedName)
+                modalWriteViewController(with: savedName)
+            }
+        } else {
+            // 저장된 이름이 없는 경우 알림창 표시
+            AlertManager.showAlert(on: self) { [weak self] name in
+                guard let self = self else { return }
+                
+                if let name = name, !name.isEmpty {
+                    print("입력 된 이름: \(name)")
+                    
+                    UserDefaults.standard.set(name, forKey: "HangdamName")       // 이름 저장
+                    UserDefaults.standard.synchronize()                          // 동기화 강제
+                    
+                    // 이름 라벨 업데이트
+                    self.mainView.updateNameLabel(name)
+                    
+                    // 메인 화면 메세지를 랜덤 메세지로 업데이트
+                    self.mainView.updateMessage(MainMessages.getRandomMessage())
+                    print("이름을 지은 후 랜덤 메세지 표시 됨")
+                    
+                    // 작성 화면으로 이동
+                    self.modalWriteViewController(with: name)
+                } else {
+                    print("이름이 입력되지 않았습니다.")
+                }
+            }
+        }
+    }
+    
     // 이미지 탭 액션
     @objc private func imageViewTap() {
         // 연속 클릭 방지
@@ -86,43 +123,6 @@ final class MainViewController: UIViewController {
         // 2초 후 다시 활성화
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
             self.mainView.circularImageView.isUserInteractionEnabled = true     // 클릭 재활성화
-        }
-    }
-    
-    // 작성 버튼 클릭 시 호출
-    @objc private func createButtonTapped() {
-        if isCreateButtonFirstTap {
-            // 이름이 없는 경우 알림창 표시하기
-            AlertManager.showAlert(on: self) { [weak self] name in
-                guard let self = self else { return }
-                
-                if let name = name, !name.isEmpty {
-                    print("입력 된 이름: \(name)")
-                    
-                    // 이름 저장
-                    UserDefaults.standard.set(name, forKey: "HangdamName")       // 이름 저장
-                    UserDefaults.standard.synchronize()                          // 동기화 강제
-                    
-                    // 이름 라벨 업데이트
-                    self.mainView.updateNameLabel(name)
-                    
-                    // 메인 화면 메세지를 랜덤 메세지로 업데이트
-                    self.mainView.updateMessage(MainMessages.getRandomMessage())
-                    print("이름을 지은 후 랜덤 메세지 표시 됨")
-                    
-                    // 작성 화면으로 이동
-                    self.modalWriteViewController(with: name)
-                } else {
-                    print("이름이 입력되지 않았습니다.")
-                }
-            }
-        } else {
-            // 이미 저장된 이름이 있는 경우에 바로 작성화면으로 이동
-            if let savedName = UserDefaults.standard.string(forKey: "HangdamName") {
-                print("저장된 이름으로 작성화면 이동함: \(savedName)")
-                mainView.updateNameLabel(savedName)
-                modalWriteViewController(with: savedName)
-            }
         }
     }
 }

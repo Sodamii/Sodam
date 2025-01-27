ae45873c0e7e9142cfc45a0aae4737dfe493efc7
Emily <emilyj4482@gmail.com>
2025-01-27 17:26:20 +0900

    ✨ [Feat] NotificationCenter를 활용한 레벨업 이벤트 전달 구현

---
+++ Sodam/Sodam/Extension/+Notification.swift
@@ -0,0 +1,12 @@
+//
+//  +Notification.swift
+//  Sodam
+//
+//  Created by EMILY on 27/01/2025.
+//
+
+import Foundation
+
+extension Notification {
+    static let levelUP = Notification.Name("levelUP")
+}

35599e965acc18d26306d9a9b40fbe9a260332a3
sy0201 <noey0201@gmail.com>
2025-01-30 16:20:29 +0900

    ♻️ [Refactor] 사용자가 선택한 시간에 매일 알림 메서드 리팩토링

--- Sodam/Sodam/Manager/LocalNotificationManager.swift
+++ Sodam/Sodam/Manager/LocalNotificationManager.swift
@@ -5,46 +5,37 @@
 //  Created by 박시연 on 1/22/25.
 //
 
-import Foundation
 import UserNotifications
+import UIKit
 
 final class LocalNotificationManager {
     static let shared = LocalNotificationManager()
     
     private init() {}
     
-    /// 특정 시점에 알람 설정(사용자가 선택한 날짜)
+    /// 특정 시점에 알람 설정(매일, 사용자가 선택한 시간)
     /// - Parameters:
-    ///   - title: Push Notification에 표시할 제목
-    ///   - body: Push Notification에 표시할 본문
     ///   - time: 사용자가 설정한 알림 시간
-    ///   - seconds: 현재로부터 seconds초 이후에 알림을 보냅니다. 0보다 커야하며 1이하 실수도 가능
-    ///   - identifier: 실행 취소, 알림 구분 등에 사용되는 식별자입니다. "TEST_NOTI" 형식으로 작성
-    func pushReservedNotification(title: String,
-                                  body: String,
-                                  time: Date,
-                                  seconds: Double,
-                                  identifier: String) {
+    func setReservedNotification(_ time: Date) {
+        let content = UNMutableNotificationContent()
+        content.title = "Sodam"  // Push Notification에 표시할 제목
+        content.body = "소소한 행복을 적어 행담이를 키워주세요."  // Push Notification에 표시할 본문
+        content.sound = .default
+        content.badge = NSNumber(value: UIApplication.shared.applicationIconBadgeNumber + 1) // 기존 뱃지 숫자에서 1씩 증가
         
-        // 알림 내용, 설정
-        let notificationContent = UNMutableNotificationContent()
-        notificationContent.title = title
-        notificationContent.body = body
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.hour, .minute], from: time)
         
-        // 알림이 발송될 정확한 시간 설정
-        let triggerComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: time)
-        // 사용자가 지정한 `time`을 기반으로 알림 트리거를 설정
-        let trigger = UNCalendarNotificationTrigger(dateMatching: triggerComponents, repeats: true)
+        let trigger = UNCalendarNotificationTrigger(dateMatching: components, repeats: true) // ✅ 매일 설정한 시간에 울리게 설정
+        
+        // identifier: 실행 취소, 알림 구분 등에 사용되는 식별자
+        let request = UNNotificationRequest(identifier: "SelectedTimeNotification", content: content, trigger: trigger)
 
-        // 알림 요청 생성
-        let request = UNNotificationRequest(identifier: identifier,
-                                            content: notificationContent,
-                                            trigger: trigger)
-        
-        // 알림 등록
         UNUserNotificationCenter.current().add(request) { error in
             if let error = error {
-                print("Notification Error: ", error)
+                print("❌ 알림 등록 실패: \(error.localizedDescription)")
+            } else {
+                print("✅ 알림 등록 성공! 예약 시간: \(components.hour ?? 0):\(components.minute ?? 0)")
             }
         }
     }

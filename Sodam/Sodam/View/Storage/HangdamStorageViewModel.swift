32232e54e5bc248cd3f45af7a1bec67d8d3256cd
Emily <emilyj4482@gmail.com>
2025-02-14 01:10:58 +0900

    ♻️ [Refactor] 행담이 보관함 뷰 MVVM 리팩토링

--- Sodam/Sodam/View/Storage/HangdamStorageViewModel.swift
+++ Sodam/Sodam/View/Storage/HangdamStorageViewModel.swift
@@ -8,16 +8,36 @@
 import Combine
 
 final class HangdamStorageViewModel: ObservableObject {
-    @Published var storedHangdams: [HangdamDTO]
+    
+    @Published var hangdamGridStores: [HangdamGridStore] = []
+    @Published var happinessListViewModels: [HappinessListViewModel] = []
     
     private let hangdamRepository: HangdamRepository
     
     init(hangdamRepository: HangdamRepository) {
         self.hangdamRepository = hangdamRepository
-        self.storedHangdams = hangdamRepository.getSavedHangdams()
+        loadHangdams()
     }
     
     func loadHangdams() {
-        self.storedHangdams = hangdamRepository.getSavedHangdams()
+        let hangdams = hangdamRepository.getSavedHangdams()
+
+        self.hangdamGridStores = hangdams.map { hangdamDTO in
+            guard let name = hangdamDTO.name,
+                  let startDate = hangdamDTO.startDate,
+                  let endDate = hangdamDTO.endDate
+            else { return HangdamGridStore(name: "이름잃은담이", dateString: "") }
+            
+            return HangdamGridStore(name: name, dateString: "\(startDate) ~ \(endDate)")
+        }
+        
+        self.happinessListViewModels = hangdams.map { hangdamDTO in
+            return HappinessListViewModel(hangdam: hangdamDTO)
+        }
     }
+}
+
+struct HangdamGridStore {
+    let name: String
+    let dateString: String
 }

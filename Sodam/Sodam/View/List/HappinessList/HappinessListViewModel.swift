6e8f37ec765f7e3fb16ac89cb6e52729584eb4d8
jbnong07 <jbnong07@naver.com>
2025-02-17 02:39:57 +0900

    ♻️ [Refactor] MapperFacotory

--- Sodam/Sodam/View/List/HappinessList/HappinessListViewModel.swift
+++ Sodam/Sodam/View/List/HappinessList/HappinessListViewModel.swift
@@ -5,6 +5,8 @@
 //  Created by 박진홍 on 1/26/25.
 //
 
+// TODO: 에러 처리에 대한 구체적인 구현이 필요함.
+
 import Foundation
 import Combine
 import UIKit
@@ -21,9 +23,9 @@ final class HappinessListViewModel: ObservableObject {
     let listViewReloader: ListViewReloader
     let cellThumbnailFetcher: CellThumbnailFetcher
     
-    private let statusContentMapper: StatusContentMapper
-    private let listContentMappper: HappinessListContentMapper
-    private let listConfigMapper: HappinessListConfigMapper
+    private let statusContentMapper: AnyDataMapper<HangdamDTO, StatusContent>
+    private let listContentMapper: AnyDataMapper<HangdamDTO, HappinessListContent>
+    private let listConfigMapper: AnyDataMapper<[HappinessDTO], [HappinessListConfig]>
     
     init(
         detailViewOperator: DetailViewOperator,
@@ -35,15 +37,19 @@ final class HappinessListViewModel: ObservableObject {
         self.listViewReloader = listViewReloader
         self.cellThumbnailFetcher = cellThumbnailFetcher
         
-        self.statusContentMapper = mapperFactory.createStatusMapper()
-        self.listContentMappper = mapperFactory.createHappinessListContentMapper()
-        self.listConfigMapper = mapperFactory.createHappinessListConfigMapper()
+        do {
+            self.statusContentMapper = try mapperFactory.createAnyMapper(inputType: HangdamDTO.self, outputType: StatusContent.self).get()
+            self.listContentMapper = try mapperFactory.createAnyMapper(inputType: HangdamDTO.self, outputType: HappinessListContent.self).get()
+            self.listConfigMapper = try mapperFactory.createAnyMapper(inputType: [HappinessDTO].self, outputType: [HappinessListConfig].self).get()
+        } catch {
+            fatalError("[ListViewModel] 매퍼 주입 실패") // 실패한 채로 실행되는 경우가 없게 개발단계에서 확실히 확인하도록 fatalError처리 해보았음.
+        }
         
         let result = listViewReloader.reloadData()
         switch result {
         case.success(let initialData):
             self.statusContent = statusContentMapper.map(from: initialData.hangdam)
-            self.listContent = listContentMappper.map(from: initialData.hangdam)
+            self.listContent = listContentMapper.map(from: initialData.hangdam)
             self.listConfigs = listConfigMapper.map(from: initialData.happiness)
             self.isCanDelete = (initialData.hangdam.endDate == nil ? false : true)
             self.isDataError = false
@@ -60,7 +66,7 @@ final class HappinessListViewModel: ObservableObject {
     func reloadData() {
         listViewReloader.reloadData().fold { initialData in
             self.statusContent = statusContentMapper.map(from: initialData.hangdam)
-            self.listContent = listContentMappper.map(from: initialData.hangdam)
+            self.listContent = listContentMapper.map(from: initialData.hangdam)
             self.listConfigs = listConfigMapper.map(from: initialData.happiness)
             self.isCanDelete = (initialData.hangdam.endDate == nil ? false : true)
             self.isDataError = false

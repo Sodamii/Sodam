352083a06d50c270b38154e2632b24d51f88bcc5
Emily <emilyj4482@gmail.com>
2025-02-18 17:58:25 +0900

    ✨ [Feat] 폰트 고정 사이즈 정의

--- Sodam/Sodam/Model/Manager/Font/FontSize.swift
+++ Sodam/Sodam/Model/Manager/Font/FontSize.swift
@@ -13,3 +13,12 @@ enum FontSize {
     static let body: CGFloat = 16
     static let timeStamp: CGFloat = 14
 }
+
+enum FontSize2 {
+    static let title1: CGFloat = 28
+    static let title2: CGFloat = 24
+    static let subtitle: CGFloat = 20
+    static let body1: CGFloat = 18
+    static let body2: CGFloat = 16
+    static let caption: CGFloat = 14
+}

65a1ee14aa144f26c0851fdc171fd73ff0466c48
emilyj4482 <emilyj4482@gmail.com>
2025-02-19 21:12:58 +0900

    🗑️ [Delete] 사용 안할 폰트 삭제

--- Sodam/Sodam/Extension/+Font.swift
+++ Sodam/Sodam/Extension/+Font.swift
@@ -31,14 +31,4 @@ extension Font {
         let fontName = "\(CustomFont.gowunBatang.rawValue)-\(type.rawValue)" // bold, regular 중 사용가능
         return .custom(fontName, size: size)
     }
-
-    static func nanumNeo(type: FontType, size: CGFloat) -> Font {
-        let fontName = "\(CustomFont.nanumSquareNeo.rawValue)-\(type.rawValue)" // neoBold, neoLight, neoRegular 중 사용가능
-        return .custom(fontName, size: size)
-    }
-
-    static func jGaegu(type: FontType, size: CGFloat) -> Font {
-        let fontName = "\(CustomFont.jGaegujaengyi.rawValue)-\(type.rawValue)" // bold, jGaeguM, jGaeguL 중 사용가능
-        return .custom(fontName, size: size)
-    }
 }
--- Sodam/Sodam/Extension/+UIFont.swift
+++ Sodam/Sodam/Extension/+UIFont.swift
@@ -31,14 +31,4 @@ extension UIFont {
         let fontName = "\(CustomFont.gowunBatang.rawValue)-\(type.rawValue)" // bold, regular 중 사용가능
         return UIFont(name: fontName, size: size) ?? UIFont()
     }
-
-    static func nanumNeo(type: FontType, size: CGFloat) -> UIFont {
-        let fontName = "\(CustomFont.nanumSquareNeo.rawValue)-\(type.rawValue)" // neoBold, neoLight, neoRegular 중 사용가능
-        return UIFont(name: fontName, size: size) ?? UIFont()
-    }
-
-    static func jGaegu(type: FontType, size: CGFloat) -> UIFont {
-        let fontName = "\(CustomFont.jGaegujaengyi.rawValue)-\(type.rawValue)" // bold, jGaeguM, jGaeguL 중 사용가능
-        return UIFont(name: fontName, size: size) ?? UIFont()
-    }
 }
